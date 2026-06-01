c384862f156507971692e2d4a245126a12a8bb0b
yujoo <yujoo@ofkr-MacBook-Pro.local>
2026-06-01 11:35:59 +0900

    update SEO keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   title: "DOYU STUDIO",
   description: "함께 살아가는 순간들을 담습니다 | 그래픽 디자인, 웨딩 스냅, 웨딩 영상",
+  keywords: "도유스튜디오, DOYU STUDIO, 웨딩스냅, 웨딩영상, 그래픽디자인, 청첩장디자인, 굿즈제작, 식전영상, 감성영상, 프로포즈영상, 커스텀디자인, 반팔티, 그래픽, 웨딩촬영, 감성사진, 커플사진, 돌스냅, 가족사진, 포스터디자인, 엽서제작, 폰케이스제작, 마우스패드제작, 서울웨딩스냅, 경기웨딩스냅, doyu, 도유, 웨딩필름, 시네마틱영상",
   icons: {
     icon: '/favicon.png',
     shortcut: '/favicon.png',
