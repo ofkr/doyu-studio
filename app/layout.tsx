0027473ece8f63c1e3923cc429c4dc7217c68474
yujoo <yujoo@ofkr-MacBook-Pro.local>
2026-06-01 11:43:18 +0900

    update google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
   description: "함께 살아가는 순간들을 담습니다 | 그래픽 디자인, 웨딩 스냅, 웨딩 영상",
   keywords: "도유스튜디오, DOYU STUDIO, 웨딩스냅, 웨딩영상, 그래픽디자인, 청첩장디자인, 굿즈제작, 식전영상, 감성영상, 프로포즈영상, 커스텀디자인, 반팔티, 그래픽, 웨딩촬영, 감성사진, 커플사진, 돌스냅, 가족사진, 포스터디자인, 엽서제작, 폰케이스제작, 마우스패드제작, 서울웨딩스냅, 경기웨딩스냅, doyu, 도유, 웨딩필름, 시네마틱영상",
   verification: {
-    google: 'SRUqhQXVFogs10Qn1wcdKmK7WEzD17lH_F_RnKB4Dn0',
+    google: 'h1AUV3cliF3GXxa3kbE7_gq2MHFtsIqHyWXj42yVD6M',
   },
   icons: {
     icon: '/favicon.png',
